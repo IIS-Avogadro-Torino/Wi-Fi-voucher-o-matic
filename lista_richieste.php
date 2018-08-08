80c9eec23017e114f72df4d46e33eb1fb6479b96
b84an <ivan.bertotto@gmail.com>
2018-08-08 02:27:27 -0700

    update IIS Avogadro ticket system URL

--- lista_richieste.php
+++ lista_richieste.php
@@ -1,7 +1,7 @@
 <?php
 ######################################################################
 # Wi-Fi-voucher-o-matic - Wi-Fi voucher manager
-# Copyright (C) 2017 Valerio Bozzolan, Ivan Bertotto, ITIS Avogadro
+# Copyright (C) 2017 Valerio Bozzolan, Ivan Bertotto, IIS Avogadro
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,13 +13,16 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.If not, see <http://www.gnu.org/licenses/>.
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 ######################################################################
+
+##
+# Questa pagina è una presentazione delle tipologie di utenze disponibili
+##
 
 require 'load.php';
 
 Header::spawn('lista_richieste');
-
 ?>
 
 <section class="mbr-section mbr-section__container mbr-after-navbar" id="video2-1q" style="background-color: rgb(0, 0, 0); padding-top: 0px; padding-bottom: 0px;">
@@ -27,8 +30,7 @@ Header::spawn('lista_richieste');
         <div class="row">
             <div class="col-xs-12">
                 <div class="mbr-figure">
-                    <div><img src="<?php echo STATIC_ROOT ?>/images/avowifiok-1400x602.png"></div>
-
+                    <div><img src="<?php echo STATIC_ROOT ?>/images/avowifiok-1400x602.png" alt="Avo Wi-Fi logo"></div>
                 </div>
             </div>
         </div>
@@ -44,7 +46,6 @@ Header::spawn('lista_richieste');
             <div class="col-md-8 col-md-offset-2 text-xs-center">
                 <h3 class="mbr-section-title display-2">Dimmi chi sei ...</h3>
                 <div class="lead"><p>Ti connetterò :-)</p></div>
-                
             </div>
         </div>
     </div>
@@ -52,8 +53,6 @@ Header::spawn('lista_richieste');
 </section>
 
 <section class="mbr-section" id="pricing-table2-2f" style="background-color: rgb(255, 255, 255); padding-top: 40px; padding-bottom: 120px;">
-
-    
 
     <div class="mbr-section mbr-section__container mbr-section__container--middle">
       <div class="container">
@@ -71,7 +70,6 @@ Header::spawn('lista_richieste');
             <div class="col-xs-12  col-md-6 col-xl-3">
                 <div class="mbr-plan card text-xs-center">
                     <div class="mbr-plan-header card-block">
-                      
                       <div class="card-title">
                         <h3 class="mbr-plan-title">DOCENTE / ATA</h3>
                         <small class="mbr-plan-subtitle">Un anno senza limiti</small>
@@ -93,7 +91,6 @@ Header::spawn('lista_richieste');
             <div class="col-xs-12  col-md-6 col-xl-3">
                 <div class="mbr-plan card text-xs-center">
                     <div class="mbr-plan-header card-block">
-                      
                       <div class="card-title">
                         <h3 class="mbr-plan-title">STUDENTE</h3>
                         <small class="mbr-plan-subtitle">Un anno senza limiti</small>
@@ -115,7 +112,6 @@ Header::spawn('lista_richieste');
             <div class="col-xs-12  col-md-6 col-xl-3">
                 <div class="mbr-plan card text-xs-center">
                     <div class="mbr-plan-header card-block">
-                      
                       <div class="card-title">
                         <h3 class="mbr-plan-title">OSPITE</h3>
                         <small class="mbr-plan-subtitle">Benvenuti all' Avo</small>
@@ -137,7 +133,6 @@ Header::spawn('lista_richieste');
             <div class="col-xs-12  col-md-6 col-xl-3">
                 <div class="mbr-plan card text-xs-center">
                     <div class="mbr-plan-header card-block">
-                      
                       <div class="card-title">
                         <h3 class="mbr-plan-title">DISPOSITIVO</h3>
                         <small class="mbr-plan-subtitle">Registra il computer della scuola</small>
@@ -152,7 +147,7 @@ Header::spawn('lista_richieste');
                     </div>
                     <div class="mbr-plan-body card-block">
                       <div class="mbr-plan-list"><ul class="list-group list-group-flush"><li class="list-group-item">banda illimitata</li><li class="list-group-item">Connesso alla lan</li></ul></div>
-                      <div class="mbr-plan-btn"><a href="http://www.grupporete.itisavogadro.org/index.php/servizi/avo-wifi/richiesta-dispositivo" class="btn btn-black btn-black-outline">Chiedi</a></div>
+                      <div class="mbr-plan-btn"><a href="https://www.grupporete.itisavogadro.org/osticket/open.php?topicId=69" class="btn btn-black btn-black-outline">Chiedi</a></div>
                     </div>
                 </div>
             </div>
