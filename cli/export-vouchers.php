62ed100bdd7bdfc6d79f08ae632e2996353b922a
Valerio Bozzolan <gnu@linux.it>
2018-08-08 07:23:43 -0700

    improve export script to work with a filename option

--- cli/export-vouchers.php
+++ cli/export-vouchers.php
@@ -1,8 +1,14 @@
+#!/bin/bash
 # Export all the vouchers from the Ubiquiti MongoDB database
+if [ -z "$1" ]; then
+	echo "Usage:"
+	echo "	$0 FILENAME.csv"
+	exit 1
+fi
 mongoexport \
 	--host localhost:27117 \
 	--db ace \
 	--collection voucher \
 	--csv \
-	--out vouchers.csv \
+	--out "$FILENAME" \
 	--fields _id,admin_name,code,create_time,duration,for_hotspot,note,qos_overwrite,qos_rate_max_down,qos_rate_max_up,qos_usage_quota,quota,site_id,used
