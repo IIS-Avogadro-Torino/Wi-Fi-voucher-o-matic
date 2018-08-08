30b524db0eb0ca655342234307a604597d2079e3
Valerio Bozzolan <gnu@linux.it>
2018-08-08 03:36:02 -0700

    update database schema to latest production version

--- documentation/database-schema.sql
+++ documentation/database-schema.sql
@@ -1,6 +1,6 @@
--- MySQL dump 10.13  Distrib 5.5.47, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.58, for debian-linux-gnu (x86_64)
 --
--- Host: mysql.itisavogadro.org    Database: grupporete
+-- Host: mysql.itisavogadro.org    Database: avowifi18
 -- ------------------------------------------------------
 -- Server version	5.6.34-log
 
@@ -14,67 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `registrazioni_user`
---
-
-DROP TABLE IF EXISTS `registrazioni_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `registrazioni_user` (
-  `user_ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_uid` varchar(64) NOT NULL,
-  `user_type` enum('god','ata','menthor','student','alien') NOT NULL DEFAULT 'alien',
-  `user_name` varchar(32) NOT NULL,
-  `user_surname` varchar(32) NOT NULL,
-  `user_role` enum('guest','admin','superadmin') NOT NULL DEFAULT 'guest',
-  `user_password` varchar(40) DEFAULT NULL,
-  `user_active` tinyint(1) NOT NULL DEFAULT '0',
-  `user_public` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`user_ID`),
-  UNIQUE KEY `user_uid` (`user_uid`),
-  KEY `user_public` (`user_public`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `registrazioni_rel_user_voucher`
---
-
-DROP TABLE IF EXISTS `registrazioni_rel_user_voucher`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `registrazioni_rel_user_voucher` (
-  `user_ID` int(10) unsigned NOT NULL,
-  `voucher_ID` int(10) unsigned NOT NULL,
-  `rel_user_voucher_creation_date` datetime NOT NULL,
-  `rel_user_voucher_creation_user` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`user_ID`,`voucher_ID`),
-  KEY `voucher_ID` (`voucher_ID`),
-  KEY `rel_user_voucher_creation_user` (`rel_user_voucher_creation_user`),
-  CONSTRAINT `registrazioni_rel_user_voucher_ibfk_1` FOREIGN KEY (`user_ID`) REFERENCES `registrazioni_user` (`user_ID`) ON DELETE CASCADE,
-  CONSTRAINT `registrazioni_rel_user_voucher_ibfk_2` FOREIGN KEY (`voucher_ID`) REFERENCES `registrazioni_voucher` (`voucher_ID`),
-  CONSTRAINT `registrazioni_rel_user_voucher_ibfk_3` FOREIGN KEY (`rel_user_voucher_creation_user`) REFERENCES `registrazioni_user` (`user_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `registrazioni_voucher`
---
-
-DROP TABLE IF EXISTS `registrazioni_voucher`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `registrazioni_voucher` (
-  `voucher_ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `voucher_code` varchar(51) NOT NULL,
-  `voucher_type` enum('god','ata','menthor','student','alien') DEFAULT NULL,
-  `voucher_duration` int(11) DEFAULT NULL,
-  PRIMARY KEY (`voucher_ID`),
-  UNIQUE KEY `voucher_code` (`voucher_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `registrazioni_activationcode`
@@ -108,6 +47,70 @@ CREATE TABLE `registrazioni_rel_user_activationcode` (
   CONSTRAINT `registrazioni_rel_user_activationcode_ibfk_2` FOREIGN KEY (`activationcode_ID`) REFERENCES `registrazioni_activationcode` (`activationcode_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `registrazioni_rel_user_voucher`
+--
+
+DROP TABLE IF EXISTS `registrazioni_rel_user_voucher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `registrazioni_rel_user_voucher` (
+  `user_ID` int(10) unsigned NOT NULL,
+  `voucher_ID` int(10) unsigned NOT NULL,
+  `rel_user_voucher_creation_date` datetime NOT NULL,
+  `rel_user_voucher_creation_user` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`user_ID`,`voucher_ID`),
+  KEY `voucher_ID` (`voucher_ID`),
+  KEY `rel_user_voucher_creation_user` (`rel_user_voucher_creation_user`),
+  CONSTRAINT `registrazioni_rel_user_voucher_ibfk_1` FOREIGN KEY (`user_ID`) REFERENCES `registrazioni_user` (`user_ID`) ON DELETE CASCADE,
+  CONSTRAINT `registrazioni_rel_user_voucher_ibfk_2` FOREIGN KEY (`voucher_ID`) REFERENCES `registrazioni_voucher` (`voucher_ID`),
+  CONSTRAINT `registrazioni_rel_user_voucher_ibfk_3` FOREIGN KEY (`rel_user_voucher_creation_user`) REFERENCES `registrazioni_user` (`user_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `registrazioni_user`
+--
+
+DROP TABLE IF EXISTS `registrazioni_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `registrazioni_user` (
+  `user_ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_uid` varchar(64) NOT NULL,
+  `user_type` enum('god','ata','menthor','student','alien') NOT NULL DEFAULT 'alien',
+  `user_name` varchar(32) NOT NULL,
+  `user_surname` varchar(32) NOT NULL,
+  `user_role` enum('guest','admin','superadmin') NOT NULL DEFAULT 'guest',
+  `user_note` text NOT NULL,
+  `user_password` varchar(40) DEFAULT NULL,
+  `user_active` tinyint(1) NOT NULL DEFAULT '0',
+  `user_public` tinyint(1) DEFAULT '0',
+  `user_recovery_token` varchar(40) DEFAULT NULL,
+  `user_recovery_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_ID`),
+  UNIQUE KEY `user_uid` (`user_uid`),
+  KEY `user_public` (`user_public`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `registrazioni_voucher`
+--
+
+DROP TABLE IF EXISTS `registrazioni_voucher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `registrazioni_voucher` (
+  `voucher_ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `voucher_code` varchar(51) NOT NULL,
+  `voucher_type` enum('god','ata','menthor','student','alien') DEFAULT NULL,
+  `voucher_duration` int(11) DEFAULT NULL,
+  PRIMARY KEY (`voucher_ID`),
+  UNIQUE KEY `voucher_code` (`voucher_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -118,4 +121,4 @@ CREATE TABLE `registrazioni_rel_user_activationcode` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-25  3:52:52
+-- Dump completed on 2018-08-08  3:35:01
