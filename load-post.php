1853c496d24948dc2d8c05df5201f4693a2373c4
Valerio Bozzolan <gnu@linux.it>
2018-08-09 07:35:55 -0700

    refactor using define_default()

--- load-post.php
+++ load-post.php
@@ -16,22 +16,19 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 ######################################################################
 
-// Default voucher duration
-defined('DEFAULT_DURATION')
-	or define('DEFAULT_DURATION', 525600);
+// default voucher duration
+define_default( 'DEFAULT_DURATION', 525600 );
 
-// Images and other files. Because of "static" is a reserved word...
-defined('STITIC')
-	or define('STITIC', 'static');
+// images and other files. Because of "static" is a reserved word...
+define_default( 'STITIC', 'static' );
+define( 'STATIC_ROOT', ROOT    . _  . STITIC );
+define( 'STATIC_URL',  URL     . _  . STITIC );
+define( 'STATIC_PATH', ABSPATH . __ . STITIC );
 
-define('STATIC_ROOT', ROOT    . _  . STITIC);
-define('STATIC_URL',  URL     . _  . STITIC);
-define('STATIC_PATH', ABSPATH . __ . STITIC);
+// classes folder
+define( 'INCLUDES', 'includes' );
 
-// Classes folder
-define('INCLUDES', 'includes');
-
-// Autoload classes
+// autoload classes
 spl_autoload_register( function($c) {
 	$path = ABSPATH . __ . INCLUDES . __ . "class-$c.php";
 	if( is_file( $path ) ) {
@@ -39,52 +36,53 @@ spl_autoload_register( function($c) {
 	}
 } );
 
-defined('SITE_NAME')
-	or define('SITE_NAME', _("Gruppo rete Avogadro") );
+// site identity
+define_default( 'SITE_NAME', _("Gruppo rete Avogadro") );
+define_default( 'COMPANY_DOMAIN', 'itisavogadro.it' );
 
-defined('COMPANY_DOMAIN')
-	or define('COMPANY_DOMAIN', 'itisavogadro.it');
+// burocratic administrator
+define_default( 'RAUSA_EMAIL', 'prausa@itisavogadro.it' );
+define_default( 'RAUSA_NAME', 'Prof. Pietro Rausa' );
 
-defined('RAUSA_EMAIL')
-	or define('RAUSA_EMAIL', 'prausa@itisavogadro.it');
+// SMTP class
+define_default( 'NET_SMTP_PATH', '/usr/share/php/Net/SMTP.php' );
 
-defined('RAUSA_NAME')
-	or define('RAUSA_NAME', 'Prof. Pietro Rausa');
+// jQuery pathname (provided by the libjs-jquery package as default)
+define_default( 'JQUERY', '/javascript/jquery/jquery.min.js' );
 
-defined('NET_SMTP_PATH')
-	or define('NET_SMTP_PATH', '/usr/share/php/Net/SMTP.php');
+// register jQuery
+register_js( 'jquery', JQUERY );
 
-defined('JQUERY')
-	or define('JQUERY', '/javascript/jquery/jquery.min.js');
-
-register_js('jquery', JQUERY);
-
+// menu entries
 add_menu_entries( [
-	new MenuEntry('index',           ROOT,                          _("Home") ),
-	new MenuEntry('lista_richieste', ROOT . '/lista_richieste.php', _("Richiedi" )         ),
-	new MenuEntry('condizioni',      ROOT . '/#header3-b',          _("Condizioni")        ),
-	new MenuEntry('panel',           ROOT . '/Panel',               _("Admin")             ),
-	new MenuEntry('docente',         ROOT . '/docente.php',         _("Docente"),          'hidden'),
-	new MenuEntry('studente',        ROOT . '/studente.php',        _("Studente"),         'hidden'),
-	new MenuEntry('ospite',          ROOT . '/ospite.php',          _("Ospite"),           'hidden'),
-	new MenuEntry('dispositivo',     ROOT . '/dispositivo.php',     _("Dispositivo"),      'hidden'),
-	new MenuEntry('form_post',       ROOT . '/form_post.php',       _("Invio dati"),       'hidden'),
-	new MenuEntry('logout',          ROOT . '/login.php?logout=1',  _("Esci"),             'hidden'),
-	new MenuEntry('password-lost',   ROOT . '/password-lost.php',   _("Password dimenticata"), 'hidden'),
-	new MenuEntry('privacy',   'http://privacy.itisavogadro.org',   _("Privacy"),          null    , [
+	new MenuEntry( 'index',           ROOT,                          __( "Home" )                           ),
+	new MenuEntry( 'lista_richieste', ROOT . '/lista_richieste.php', __( "Richiedi" )                       ),
+	new MenuEntry( 'condizioni',      ROOT . '/#header3-b',          __( "Condizioni" )                     ),
+	new MenuEntry( 'panel',           ROOT . '/Panel',               __( "Admin" )                          ),
+	new MenuEntry( 'docente',         ROOT . '/docente.php',         __( "Docente" ),              'hidden' ),
+	new MenuEntry( 'studente',        ROOT . '/studente.php',        __( "Studente" ),             'hidden' ),
+	new MenuEntry( 'ospite',          ROOT . '/ospite.php',          __( "Ospite" ),               'hidden' ),
+	new MenuEntry( 'dispositivo',     ROOT . '/dispositivo.php',     __( "Dispositivo" ),          'hidden' ),
+	new MenuEntry( 'form_post',       ROOT . '/form_post.php',       __( "Invio dati" ),           'hidden' ),
+	new MenuEntry( 'logout',          ROOT . '/login.php?logout=1',  __( "Esci" ),                 'hidden' ),
+	new MenuEntry( 'password-lost',   ROOT . '/password-lost.php',   __( "Password dimenticata"),  'hidden' ),
+	new MenuEntry( 'privacy',   'http://privacy.itisavogadro.org',   __( "Privacy" ),               null,   [
 		'intag' => 'target="_blank"',
 	] ),
 
 ] );
 
-register_permissions('admin', [
-	'administrate'
+// administrator permissions
+register_permissions( 'admin', [
+	'administrate',
 ] );
 
-inherit_permissions('superadmin', 'admin', [
+// super administrator permissions
+inherit_permissions( 'superadmin', 'admin', [
 	'view-all-users',
 	'register_whatever_ata_mail',
 	'register_god',
 ] );
 
+// load some site functions
 require ABSPATH . __ . INCLUDES . __ . 'functions.php';
