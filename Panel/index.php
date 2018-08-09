6244014a293414cffee0751ae4994b0193f17e6f
Valerio Bozzolan <gnu@linux.it>
2018-08-09 01:12:05 -0700

    remove explicit JavaScript type and avoid deprecated jQuery.ready()

--- Panel/index.php
+++ Panel/index.php
@@ -563,9 +563,9 @@ $VOUCHERS_GOD_FREE     = $count_available( Voucher::GOD     );
 	</footer>
 
 	<!--   Core JS Files   -->
-	<script src="assets/js/jquery-3.1.0.min.js" type="text/javascript"></script>
-	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
-	<script src="assets/js/material.min.js" type="text/javascript"></script>
+	<script src="assets/js/jquery-3.1.0.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/material.min.js"></script>
 
 	<!--  Charts Plugin -->
 	<script src="assets/js/chartist.min.js"></script>
@@ -579,8 +579,8 @@ $VOUCHERS_GOD_FREE     = $count_available( Voucher::GOD     );
 	<!-- Material Dashboard DEMO methods, don't include it in your project! -->
 	<script src="assets/js/demo.js"></script>
 
-	<script type="text/javascript">
-	$(document).ready( function () {
+	<script>
+	$( function () {
 		// Javascript method's body can be found in assets/js/demos.js
         	demo.initDashboardPageCharts();
 	} );
